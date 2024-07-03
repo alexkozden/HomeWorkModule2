dab285141994d9d569db7597dff6f706a1d27fb1
alexkozden <aleksey.kozlov.2000@mail.ru>
2024-07-03 04:53:14 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = 24
+second = 12 + 12
+third = 48 / 2
+
+if first == second and first == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
